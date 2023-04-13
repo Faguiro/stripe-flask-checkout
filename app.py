8dbcdbab889e95e6891b6ac012405a82e2607ecf
faguiro <faguiro2005@gmail.com>
2023-04-13 16:00:39 -0300

    :boom: Main revision

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import os
 from flask import Flask, render_template, abort, redirect, request
 import stripe
-import dotenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 stripe.api_key = os.environ['STRIPE_SECRET_KEY']
 
 products = {
     'Suporte total': {
-        'name': 'Pack full -  Aprenda Python',
+        'name': 'Pack full - Aprenda Python',
         'price': 200000,
     },
     'Suporte por horas': {
-        'name': 'Pack  horas - Aprenda Python',
+        'name': 'Pack horas - Aprenda Python',
         'price': 10000,
         'por': 'hora',
         'adjustable_quantity': {
@@ -22,6 +24,7 @@ products = {
         },
     },
 }
+
 @app.route('/')
 def index():
     return render_template('index.html', products=products)
@@ -31,35 +34,37 @@ def order(product_id):
     if product_id not in products:
         abort(404)
 
-    checkout_session = stripe.checkout.Session.create(
-        line_items=[
-            {
-                'price_data': {
-                    'product_data': {
-                        'name': products[product_id]['name'],
-                    },
-                    'unit_amount': products[product_id]['price'],
-                    'currency': 'BRL',
-                },
-                'quantity': 1,
-                'adjustable_quantity': products[product_id].get(
-                    'adjustable_quantity', {'enabled': False}),
+    product = products[product_id]
+
+    line_item = {
+        'price_data': {
+            'product_data': {
+                'name': product['name'],
             },
-        ],
+            'unit_amount': product['price'],
+            'currency': 'BRL',
+        },
+        'quantity': 1,
+        'adjustable_quantity': product.get('adjustable_quantity', {'enabled': False}),
+    }
+
+    checkout_session = stripe.checkout.Session.create(
+        line_items=[line_item],
         payment_method_types=['card'],
         mode='payment',
         success_url=request.host_url + 'order/success',
         cancel_url=request.host_url + 'order/cancel',
     )
     return redirect(checkout_session.url)
+
 @app.route('/order/success')
 def success():
     return render_template('success.html')
 
-
 @app.route('/order/cancel')
 def cancel():
     return render_template('cancel.html')
+
 @app.route('/event', methods=['POST'])
 def new_event():
     event = None
@@ -74,11 +79,11 @@ def new_event():
         abort(400)
 
     if event['type'] == 'checkout.session.completed':
-      session = stripe.checkout.Session.retrieve(
-          event['data']['object'].id, expand=['line_items'])
-      print(f'Sale to {session.customer_details.email}:')
-      for item in session.line_items.data:
-          print(f'  - {item.quantity} {item.description} '
-                f'${item.amount_total/100:.02f} {item.currency.uppor()}')
+        session = stripe.checkout.Session.retrieve(
+            event['data']['object'].id, expand=['line_items'])
+        print(f'Sale to {session.customer_details.email}:')
+        for item in session.line_items.data:
+            print(f'  - {item.quantity} {item.description} '
+                  f'${item.amount_total/100:.02f} {item.currency.upper()}')
 
     return {'success': True}
